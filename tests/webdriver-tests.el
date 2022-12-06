afbe8ebcf6af5d0765111da1895c882202da9bc4
Mauro Aranda <maurooaranda@gmail.com>
2022-12-06 10:40:01 -0300

    Add test infrastructure
    * tests/webdriver-tests.el (webdriver-test-service)
    (webdriver-test-session): New variables.
    (webdriver-test-with-session): New macro.
    (webdriver-test-session-start-and-stop)
    (webdriver-test-session-navigation): New tests.

--- tests/webdriver-tests.el
+++ tests/webdriver-tests.el
@@ -21,6 +21,27 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'webdriver)
+
+(defvar webdriver-test-service (make-instance 'webdriver-service-firefox
+                                              :executable "geckodriver")
+  "Service to use when testing.")
+(webdriver-service-start webdriver-test-service)
+
+(defvar webdriver-test-session nil "Session to use when testing.")
+
+(defmacro webdriver-test-with-session (&rest body)
+  "Execute BODY wrapped around a session start and session stop logic."
+  `(let ((webdriver-test-session (make-instance 'webdriver-session
+                                                :service webdriver-test-service
+                                                :requested-capabilities
+                                                '(:alwaysMatch (:moz:firefoxOptions
+                                                                (:args ["-headless"]))))))
+     (webdriver-session-start webdriver-test-session)
+     (unwind-protect
+         ,@body
+       (webdriver-session-stop webdriver-test-session))))
+
 (ert-deftest webdriver-test-service-start-and-stop ()
   "Test that starting and stopping a service works."
   (let ((service (make-instance 'webdriver-service-firefox :executable "geckodriver"
@@ -41,6 +62,23 @@
                   (url-retrieve-synchronously (webdriver-service-url service))
                   nil)
               (file-error t)))))
+
+(ert-deftest webdriver-test-session-start-and-stop ()
+  "Test that starting and stopping a session works."
+  (webdriver-test-with-session
+   (should (oref webdriver-test-session id)))
+  (should-not (oref webdriver-test-session id)))
+
+(ert-deftest webdriver-test-session-navigation ()
+  "Test session navigation."
+  (webdriver-test-with-session
+   (webdriver-goto-url webdriver-test-session "https://www.example.org/")
+   (should (string= (webdriver-get-current-url webdriver-test-session)
+                    "https://www.example.org/"))
+   (webdriver-refresh webdriver-test-session)
+   (should (string= (webdriver-get-title webdriver-test-session) "Example Domain"))))
+
+(webdriver-service-stop webdriver-test-service)
 
 (provide 'webdriver-tests)
 ;;; webdriver-tests.el ends here
